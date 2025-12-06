60b14afbcca63952151dfff6e14acd4c5bd23691
unksnow <ba.martinezm@duocuc.cl>
2025-12-05 21:41:35 -0300

    backendV2

--- server.js
+++ server.js
@@ -7,58 +7,69 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// Configura tu conexión a MySQL
+
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',      // Tu usuario de MySQL
-    password: '123456', // Tu contraseña de MySQL
+    user: 'root',      
+    password: '123456', 
     database: 'proyecto_semestral_db'
 });
 
 db.connect(err => {
-    if (err) throw err;
-    console.log('Conectado a MySQL');
+    if (err) {
+        console.error('Error conectando a MySQL:', err);
+    } else {
+        console.log('Conectado a MySQL');
+    }
 });
 
-// Ruta para Registrar Usuario
 app.post('/register', (req, res) => {
     const { email, password } = req.body;
+    
+
+    if (!email || !password) {
+        return res.status(400).send({ message: 'Faltan datos' });
+    }
+
     const sql = 'INSERT INTO users (email, password) VALUES (?, ?)';
     
     db.query(sql, [email, password], (err, result) => {
-        if (err.code === 'ER_DUP_ENTRY') {
-                res.status(409).send({ message: 'El usuario ya existe' });
-        } else {
-            res.status(200).send({ message: 'Usuario registrado exitosamente', userId: result.insertId });
-        }
+        if (err) {
+            if (err.code === 'ER_DUP_ENTRY') {
+                return res.status(409).send({ message: 'El usuario ya existe' });
+            }
+            console.log("Error SQL:", err);
+            return res.status(500).send({ message: 'Error al registrar' });
+        } 
+        
+
+        res.status(200).send({ message: 'Usuario registrado exitosamente', userId: result.insertId });
     });
 });
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
-});
 app.post('/login', (req, res) => {
     const { email, password } = req.body;
     
-    // Consultamos si existe ese usuario con esa contraseña
     const sql = 'SELECT * FROM users WHERE email = ? AND password = ?';
     
     db.query(sql, [email, password], (err, result) => {
         if (err) {
+            console.log("Error en Login:", err);
             res.status(500).send({ message: 'Error en el servidor' });
         } else {
             if (result.length > 0) {
-                // ¡Login correcto!
-                // Devolvemos el ID para que la App sepa quién es
                 res.status(200).send({ 
                     message: 'Login exitoso', 
                     userId: result[0].id,
                     email: result[0].email 
                 });
             } else {
-                // Login incorrecto
                 res.status(401).send({ message: 'Credenciales incorrectas' });
             }
         }
     });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
 });
